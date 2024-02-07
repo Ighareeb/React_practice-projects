de8a8c121e5c42bb5b15ed293dc54b646a5f744e
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-02-07 20:14:54 +0400

    ALL DATA TABLES COMPLETEDcreated some of the pages for the app /scenes/invoices/index.jsx with checkbox and datagrid like previous pages  TESTED COMPONENT RENDERING OK (note: column sorting is default for both)

--- admin-dashboard/src/scenes/invoices/index.jsx
+++ admin-dashboard/src/scenes/invoices/index.jsx
@@ -1,16 +1,16 @@
-import { Box, useTheme } from '@mui/material';
-import { DataGrid, GridToolbar } from '@mui/x-data-grid'; //https://mui.com/x/react-data-grid/
+import { Box, useTheme, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid'; //https://mui.com/x/react-data-grid/
 import { tokens } from '../../theme';
-import { mockDataContacts } from '../../data/mockData';
+import { mockDataInvoices } from '../../data/mockData';
 import Header from '../../components/Header';
 
-export default function Team() {
+export default function Invoices() {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
 
 	const columns = [
-		{ field: 'id', headerName: 'ID', flex: 0.5 },
-		{ field: 'registrarId', headerName: 'Registrar ID' },
+		{ field: 'id', headerName: 'ID' },
+
 		{
 			field: 'name',
 			headerName: 'Name',
@@ -18,11 +18,19 @@ export default function Team() {
 			cellClassName: 'name-column--cell',
 		},
 		{
-			field: 'age',
-			headerName: 'Age',
-			type: 'number',
-			headerAlign: 'left',
-			align: 'left',
+			field: 'email',
+			headerName: 'Email',
+			flex: 1,
+		},
+		{
+			field: 'cost',
+			headerName: 'Cost',
+			flex: 1,
+			renderCell: (params) => (
+				<Typography color={colors.greenAccent[500]}>
+					${params.row.cost}
+				</Typography>
+			),
 		},
 		{
 			field: 'phone',
@@ -30,33 +38,15 @@ export default function Team() {
 			flex: 1,
 		},
 		{
-			field: 'email',
-			headerName: 'Email',
-			flex: 1,
-		},
-		{
-			field: 'address',
-			headerName: 'Address',
-			flex: 1,
-		},
-		{
-			field: 'city',
-			headerName: 'City',
-			flex: 1,
-		},
-		{
-			field: 'zipCode',
-			headerName: 'ZipCode',
+			field: 'date',
+			headerName: 'Date',
 			flex: 1,
 		},
 	];
 	return (
 		// need to define Box and height explicitly for it to display in the UI properly (initially height is 100%)
 		<Box m="20px">
-			<Header
-				title="CONTACTS"
-				subtitle="List of Contacts for Future Reference"
-			/>
+			<Header title="INVOICES" subtitle="List of Invoice Balances" />
 			<Box
 				m="40px 0 0 0"
 				height="75vh"
@@ -77,17 +67,12 @@ export default function Team() {
 						borderTop: 'none',
 						backgroundColor: colors.blueAccent[700],
 					},
-					//this is a GridToolbar component that will allow filtering
-					'& .MuiDataGrid-toolbarContainer .MuiButton-text': {
-						color: `${colors.grey[100]} !important`,
+					'& .MuiCheckbox-root': {
+						color: `${colors.greenAccent[200]} !important`,
 					},
 				}}
 			>
-				<DataGrid
-					rows={mockDataContacts}
-					columns={columns}
-					components={{ Toolbar: GridToolbar }}
-				/>
+				<DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
 			</Box>
 		</Box>
 	);
