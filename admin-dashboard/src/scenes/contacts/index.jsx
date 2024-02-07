30c3e6a31098f138963d47914f97e828215e7ba9
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-02-07 18:21:21 +0400

    created some of the pages for the app /scenes/contacts/index.jsx with DatagridToolbar modifying columns being displayed and UI display and filtering data  TESTED COMPONENT RENDERING OK (note: column sorting is default for both)

--- admin-dashboard/src/scenes/contacts/index.jsx
+++ admin-dashboard/src/scenes/contacts/index.jsx
@@ -1,10 +1,7 @@
-import { Box, Typography, useTheme } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid'; //https://mui.com/x/react-data-grid/
+import { Box, useTheme } from '@mui/material';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'; //https://mui.com/x/react-data-grid/
 import { tokens } from '../../theme';
-import { mockDataTeam } from '../../data/mockData';
-import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
-import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
-import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
+import { mockDataContacts } from '../../data/mockData';
 import Header from '../../components/Header';
 
 export default function Team() {
@@ -12,7 +9,8 @@ export default function Team() {
 	const colors = tokens(theme.palette.mode);
 
 	const columns = [
-		{ field: 'id', headerName: 'ID' },
+		{ field: 'id', headerName: 'ID', flex: 0.5 },
+		{ field: 'registrarId', headerName: 'Registrar ID' },
 		{
 			field: 'name',
 			headerName: 'Name',
@@ -38,42 +36,28 @@ export default function Team() {
 			flex: 1,
 		},
 		{
-			field: 'access',
-			headerName: 'Access',
+			field: 'address',
+			headerName: 'Address',
 			flex: 1,
-			//renderCell prop allows return of custom component for each cell in the column depending on function logic (in this case 'access')
-			renderCell: ({ row: { access } }) => {
-				return (
-					<Box
-						width="60%"
-						m="0 auto"
-						p="5px"
-						display="flex"
-						justifyContent="center"
-						borderRadius="4px"
-						// style background color based on access
-						backgroundColor={
-							access === 'admin'
-								? colors.greenAccent[600]
-								: colors.greenAccent[700]
-						}
-					>
-						{/* use different icons based on access */}
-						{access === 'admin' && <AdminPanelSettingsOutlinedIcon />}
-						{access === 'manager' && <SecurityOutlinedIcon />}
-						{access === 'user' && <LockOpenOutlinedIcon />}
-						<Typography color={colors.grey[100]} sx={{ ml: '5px' }}>
-							{access}
-						</Typography>
-					</Box>
-				);
-			},
+		},
+		{
+			field: 'city',
+			headerName: 'City',
+			flex: 1,
+		},
+		{
+			field: 'zipCode',
+			headerName: 'ZipCode',
+			flex: 1,
 		},
 	];
 	return (
 		// need to define Box and height explicitly for it to display in the UI properly (initially height is 100%)
 		<Box m="20px">
-			<Header title="TEAM" subtitle="Managing the Team Members" />
+			<Header
+				title="Contacts"
+				subtitle="List of Contacts for Future Reference"
+			/>
 			<Box
 				m="40px 0 0 0"
 				height="75vh"
@@ -94,9 +78,17 @@ export default function Team() {
 						borderTop: 'none',
 						backgroundColor: colors.blueAccent[700],
 					},
+					//this is a GridToolbar component that will allow filtering
+					'& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+						color: `${colors.grey[100]} !important`,
+					},
 				}}
 			>
-				<DataGrid rows={mockDataTeam} columns={columns} />
+				<DataGrid
+					rows={mockDataContacts}
+					columns={columns}
+					components={{ Toolbar: GridToolbar }}
+				/>
 			</Box>
 		</Box>
 	);
