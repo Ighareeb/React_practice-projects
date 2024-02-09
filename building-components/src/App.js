c29d0705f5039dc526633ad90ed74e4171bb313d
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-02-09 15:25:12 +0400

    (4) Image Slider --> components/image-slider/index.jsx + styles.css TESTED OK!

--- building-components/src/App.js
+++ building-components/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 // import Accordian from './components/accordian';
 // import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+// import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
 	return (
 		<div className="App">
 			{/* <Accordian /> */}
 			{/* <RandomColor /> */}
-			<StarRating />
+			{/* <StarRating /> */}
+			<ImageSlider
+				url={'https://picsum.photos/v2/list'}
+				page={'1'}
+				limit={'10'}
+			/>
 		</div>
 	);
 }
