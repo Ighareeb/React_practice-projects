6567338fc1f661b959c58915ed89c374bc7c7f56
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-02-11 09:54:40 +0400

    STARTED Weather App vite app --> added components/weather.jsx & /search.jsx(util for weather) + added styles in App.css

---
+++ weather_app/src/App.jsx
@@ -0,0 +1,10 @@
+import Weather from './components/weather';
+import './App.css';
+
+export default function App() {
+	return (
+		<div className="App">
+			<Weather />
+		</div>
+	);
+}
---
+++ weather_app/src/components/utils/search.jsx
@@ -0,0 +1,5 @@
+//utility function for Weather functional component
+
+export default function search({ search, setSearch, handleSearch }) {
+	return <div>search</div>;
+}
