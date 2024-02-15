dc19b286f6084e9bfb34d580e7a27d0671f2edbc
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-02-15 13:51:02 +0400

    CLIENT App.js --> imported ThemeProvider to wrap + created routes (+ added folders for src/scenes folders with index.js files for components for those routes - worked on some of the scenes folder (dashboard, layout) + created components/FlexBetween.jsx (styled component - made Box for mui components custom Flex) + completed component/Navbar.jsx scenes: layout/index.js =  added some imports, plus started layout adding Navbar and Outlet dashboard/index.js = skeleton setup

--- admin-dashboard_MERN/client/src/App.js
+++ admin-dashboard_MERN/client/src/App.js
@@ -1,5 +1,60 @@
+//set up/reset baseline defaults to normalize across browsers + provider wraps components with theme
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+// allows access of data from redux store within react components (src/state/index.js)
+import { useSelector } from 'react-redux';
+
+// function to create custom theme (define/use light-dark theme)
+import { createTheme } from '@mui/material/styles';
+
+//custom theme module light-dark mode
+import { themeSettings } from 'theme';
+
+import { useMemo } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from 'scenes/layout';
+import Dashboard from 'scenes/dashboard';
+import Products from 'scenes/products';
+import Customers from 'scenes/customers';
+import Transactions from 'scenes/transactions';
+import Geography from 'scenes/geography';
+import Overview from 'scenes/overview';
+import Daily from 'scenes/daily';
+import Monthly from 'scenes/monthly';
+import Breakdown from 'scenes/breakdown';
+import Admin from 'scenes/admin';
+import Performance from 'scenes/performance';
 function App() {
-	return <div className="app"></div>;
+	const mode = useSelector((state) => state.global.mode);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); //[mode] is dependency array for useMemo
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						{/* Layout component (with Navbar+Sidebar) is parent/nests routes --> base layout for all components */}
+						{/* needs outlet to show {children} components */}
+						<Route element={<Layout />}>
+							{/* replace current entry in history stack instead of adding a new one --> doesnt remain in user browser history so can't use back button */}
+							<Route path="/" element={<Navigate to="/dashboard" replace />} />
+							<Route path="/dashboard" element={<Dashboard />} />
+							<Route path="/products" element={<Products />} />
+							<Route path="/customers" element={<Customers />} />
+							<Route path="/transaction" element={<Transactions />} />
+							<Route path="/geography" element={<Geography />} />
+							<Route path="/overview" element={<Overview />} />
+							<Route path="/daily" element={<Daily />} />
+							<Route path="/monthly" element={<Monthly />} />
+							<Route path="/breakdown" element={<Breakdown />} />
+							<Route path="/admin" element={<Admin />} />
+							<Route path="/performance" element={<Performance />} />
+						</Route>
+					</Routes>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
