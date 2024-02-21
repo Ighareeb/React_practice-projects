b1c6561f31694366174deefcc659318a5a8f92d6
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-02-21 20:13:49 +0400

    created src/pages [About, Home, Profile, SignIn, SignUp].jsx - profile,SignIn SignUp not completed completed main.jsx file - (wrapped App.jsx with store provider and persistor) completed App.jsx -react router setup for routes and added components to render

--- mern-auth-practice-app/client/src/App.jsx
+++ mern-auth-practice-app/client/src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+
 export default function App() {
 	return (
-		<>
-			<div>App</div>
-		</>
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route element={<PrivateRoute />}>
+					<Route path="/profile" element={<Profile />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
---
+++ mern-auth-practice-app/client/src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
+
+export default function SignUp() {
+	return <div>SignUp</div>;
+}
