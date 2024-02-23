365280545c2f3f0f18caf0878b47295acf4276fd
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-02-23 08:58:28 +0400

    setup BrowserRouter, QueryProvider and AuthProvider in src/main.tsx setup routes in src/App.tsx

--- social-media-app/src/App.tsx
+++ social-media-app/src/App.tsx
@@ -1,7 +1,47 @@
+import { Routes, Route } from "react-router-dom";
+
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
+import { Toaster } from "@/components/ui/toaster";
+
+import "./global.css";
+
 export default function App() {
-	return (
-		<>
-			<div>hello</div>
-		</>
-	);
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/edit-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </main>
+  );
 }
--- social-media-app/src/main.tsx
+++ social-media-app/src/main.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+import { AuthProvider } from "@/context/AuthContext";
+import { QueryProvider } from "@/lib/react-query/QueryProvider";
+
+import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <QueryProvider>
+          <App />
+        </QueryProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
